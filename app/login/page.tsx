fdf967eb2726e535cf864e0532c8c7828a391eae
Christian Ing Sunardi <christianingsunardi@gmail.com>
2024-09-26 18:37:43 +0700

    feat: add login form validations using react-hook-form and zod

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,11 +1,29 @@
 "use client";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-import { useEffect, useState } from "react";
-import clsx from "clsx";
+const loginSchema = z.object({
+  email: z.string().min(1, "Email is required").email("Invalid email address"),
+  password: z.string().min(1, "Password is required"),
+});
 
-import { Button } from "./components/buton";
+type LoginFormInputs = z.infer<typeof loginSchema>;
 
 export default function Page() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginFormInputs>({
+    resolver: zodResolver(loginSchema),
+    mode: "onChange",
+  });
+
+  function onSubmit(data: LoginFormInputs): void {
+    console.log("Form Data: ", data);
+  }
+
   return (
     <div className="h-full flex flex-col items-center justify-center">
       <img
@@ -16,28 +34,39 @@ export default function Page() {
         height="150"
       />
       <div className="w-full max-w-sm">
-        <form>
-          <div className="mb-6">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="mb-2">
             <label className="my-2 block text-slate-700 font-semibold">
               Email
             </label>
             <input
               type="email"
               id="email"
-              className="ring-1 ring-slate-400 appearance-none rounded-md p-2 w-full"
+              {...register("email")}
+              className="ring-1 ring-slate-400 appearance-none rounded-md px-4 py-2 w-full"
             />
+            <p className="text-red-500 text-sm mt-1 h-4">
+              {errors.email?.message}
+            </p>
           </div>
-          <div className="mb-6">
+          <div className="mb-2">
             <label className="my-2 block text-slate-700 font-semibold">
               Password
             </label>
             <input
               type="password"
               id="password"
-              className="ring-1 ring-slate-400 appearance-none rounded-md p-2 w-full"
+              {...register("password")}
+              className="ring-1 ring-slate-400 appearance-none rounded-md px-4 py-2 w-full"
             />
+            <p className="text-red-500 text-sm mt-1 h-4">
+              {errors.password?.message}
+            </p>
           </div>
-          <button className="w-full my-2 bg-slate-900 text-white font-semibold rounded-md p-2 hover:bg-slate-700">
+          <button
+            type="submit"
+            className="w-full my-2 bg-slate-900 text-white font-semibold rounded-md p-2 hover:bg-slate-700"
+          >
             Login to account
           </button>
         </form>
