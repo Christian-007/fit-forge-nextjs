c2aeaaffa08818f763f734a8ccf2e53ae27b75dd
Christian-007 <christianingsunardi@gmail.com>
2025-06-05 22:02:05 +0700

    feat: add full name field and make a POST /users request

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import Link from 'next/link';
 import { zodResolver } from '@hookform/resolvers/zod';
@@ -9,6 +10,7 @@ import { SignUpFormInputs, signUpSchema } from './sign-up.schema';
 
 import { TopbarConfig } from '@/app/shared/components';
 import { TopbarConfigSetter } from '@/app/shared/components/topbar/topbar-config-setter';
+import { LoadingIndicator } from '@/app/login/components/loading/loading';
 
 const topbarConfig: TopbarConfig = {
   left: (
@@ -28,9 +30,37 @@ export default function Page() {
     resolver: zodResolver(signUpSchema),
     mode: 'onChange',
   });
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
-  function onSubmit(data: SignUpFormInputs): void {
-    console.log('submitted data: ', data);
+  async function onSubmit(formData: SignUpFormInputs): Promise<void> {
+    setIsLoading(true);
+
+    try {
+      const res = await fetch('http://localhost:4000/users', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          name: formData.fullName,
+          email: formData.email,
+          password: formData.password,
+        }),
+      });
+      if (!res.ok) {
+        const err = await res.json();
+        console.log('Error: ', err.message);
+        setIsLoading(false);
+        return;
+      }
+
+      const data = await res.json();
+      setIsLoading(false);
+      console.log('data: ', data);
+    } catch (error) {
+      setIsLoading(false);
+      console.log('Something went wrong.');
+    }
   }
 
   return (
@@ -39,6 +69,21 @@ export default function Page() {
       <div className="flex h-full flex-col items-center justify-center">
         <div className="w-full max-w-sm">
           <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="mb-4">
+              <label htmlFor="fullName" className="my-2 block text-[#96C4A8]">
+                Full Name
+              </label>
+              <input
+                type="text"
+                id="fullName"
+                placeholder="e.g. John Doe"
+                className="w-full rounded-md bg-[#2645] px-4 py-3 text-[#96C4A8] ring-1 ring-[#2645] placeholder:text-gray-400 focus:outline-none focus:ring-2 focus:ring-[#38E078]"
+                {...register('fullName')}
+              />
+              {errors.fullName && (
+                <p className="mt-1 h-4 text-sm text-red-500">{errors.fullName?.message}</p>
+              )}
+            </div>
             <div className="mb-4">
               <label htmlFor="email" className="my-2 block text-[#96C4A8]">
                 Email
@@ -85,10 +130,11 @@ export default function Page() {
               )}
             </div>
             <button
+              disabled={isLoading}
               type="submit"
               className="block w-full rounded-full bg-[#38E078] p-4 text-center text-[16px] font-bold text-[#122117] hover:opacity-70"
             >
-              Sign Up
+              {isLoading ? <LoadingIndicator text="Signing up..." /> : 'Sign Up'}
             </button>
             <div className="mt-4 text-center text-sm text-[#96C4A8]">
               <span>Already have an account?</span>
--- app/sign-up/sign-up.schema.ts
+++ app/sign-up/sign-up.schema.ts
@@ -2,6 +2,11 @@ import { z } from 'zod';
 
 export const signUpSchema = z
   .object({
+    fullName: z
+      .string()
+      .min(2, 'Full name is required')
+      .max(200, 'Full name must be below 200 characters')
+      .regex(new RegExp('^[a-zA-Z\\s]+$'), 'Full name should not contain numbers'),
     email: z.string().min(1, 'Email is required').email('Invalid email address'),
     password: z.string().min(1, 'Password is required'),
     confirmPassword: z.string(),
