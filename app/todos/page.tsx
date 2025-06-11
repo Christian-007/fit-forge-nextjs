1fe682ffe72b2e91c7032d48e25c838e9f4f53a2
Christian-007 <christianingsunardi@gmail.com>
2025-06-11 07:42:33 +0700

    feat: style checkbox

--- app/todos/page.tsx
+++ app/todos/page.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import { PlusCircleIcon } from '@heroicons/react/24/solid';
+import { EllipsisVerticalIcon, PlusCircleIcon } from '@heroicons/react/24/solid';
 
 import { BottomBar, TopbarConfig } from '@/app/shared/components';
 import { TopbarConfigSetter } from '@/app/shared/components/topbar/topbar-config-setter';
@@ -81,18 +81,35 @@ export default function Page() {
     }
 
     return (
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.title}</li>
-        ))}
-      </ul>
+      <>
+        <div className="px-8 py-4">
+          <fieldset className="space-y-3">
+            {todos.map((todo) => (
+              <div key={todo.id} className="grid grid-cols-[1fr_24px] items-center gap-6 space-x-4">
+                <label className="peer grid grid-cols-[auto_1fr] items-center gap-3 rounded-md px-2 hover:bg-gray-100 dark:hover:bg-white/5">
+                  <input
+                    className="peer size-3.5 appearance-none rounded-sm border border-[#366347] accent-[#38E078] checked:appearance-auto"
+                    type="checkbox"
+                  />
+                  <span className="select-none text-gray-700 peer-checked:text-gray-400 peer-checked:line-through dark:text-gray-300">
+                    {todo.title}
+                  </span>
+                </label>
+                <button className="peer-has-checked:hidden size-[26px] rounded-md p-1 hover:bg-white/5">
+                  <EllipsisVerticalIcon className="size-5 text-white" />
+                </button>
+              </div>
+            ))}
+          </fieldset>
+        </div>
+      </>
     );
   };
 
   return (
     <>
       <TopbarConfigSetter config={topbarConfig} />
-      <div className="flex h-full flex-col items-center justify-center">{renderMainContent()}</div>
+      <div className="flex h-full flex-col justify-center">{renderMainContent()}</div>
       <BottomBar />
     </>
   );
